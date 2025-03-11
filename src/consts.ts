039d2d24359eedbf496dfc361ebd8c7457f0800a
BronzeQQ <trainedbronze@gmail.com>
2025-03-11 09:26:31 +0100

    Convertimos a componentes de los svg y cambiamos el nombre del sitio

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'RaMarinS';
+export const SITE_TITLE = 'RafaMS';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const GITHUB_REPO = 'https://github.com/dravel04';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/rafmarsan';
